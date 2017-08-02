edf5c9f631b7a3dd704268af9b4f77721a9a0e90
Ozhmegov Egor <egor-ozhmegov@mail.ru>
2017-08-02 17:41:25 +0300

    test

--- src/view/Team.js
+++ src/view/Team.js
@@ -5,7 +5,8 @@ export default class Team extends Component {
     constructor () {
         super();
         this.state = {
-            showModal: false,
+            showUpdatePlayerModal: false,
+            showAddPlayerModal: false,
             player:{
                 id: '',
                 name: '',
@@ -25,16 +26,29 @@ export default class Team extends Component {
         });
     }
 
-    handleOpenModal (player) {
+    handleOpenUpdatePlayerModal (player) {
         this.setState({
-            showModal: true,
+            showUpdatePlayerModal: true,
             player:player
         });
     }
 
-    handleCloseModal () {
+    handleOpenAddPlayerModal () {
         this.setState({
-            showModal: false
+            showAddPlayerModal: true,
+            player:{}
+        });
+    }
+
+    handleCloseUpdatePlayerModal () {
+        this.setState({
+            showUpdatePlayerModal: false
+        });
+    }
+
+    handleCloseAddPlayerModal () {
+        this.setState({
+            showAddPlayerModal: false
         });
     }
 
@@ -49,6 +63,7 @@ export default class Team extends Component {
         this.lastNameInput.value = '';
         this.numberInput.value = '';
         this.roleInput.value = '';
+        this.handleCloseAddPlayerModal();
     }
 
     deletePlayer(id){
@@ -57,18 +72,13 @@ export default class Team extends Component {
 
     updatePlayer(event){
         event.preventDefault();
-        console.log(this.state.player);
+        this.props.playerActions.updatePlayer(this.state.player);
+        this.handleCloseUpdatePlayerModal();
     }
 
     render() {
         return (
             <div>
-                <input type="text" placeholder="Name" ref={(input) => {this.nameInput = input}}/>
-                <input type="text" placeholder="Last Name" ref={(input) => {this.lastNameInput = input}}/>
-                <input type="text" placeholder="Number" ref={(input) => {this.numberInput = input}}/>
-                <input type="text" placeholder="Role" ref={(input) => {this.roleInput = input}}/>
-                <button onClick={this.addPlayer.bind(this)}>Add player</button>
-
                 <table>
                     <thead>
                     <tr>
@@ -77,6 +87,7 @@ export default class Team extends Component {
                         <th>Last Name</th>
                         <th>Number</th>
                         <th>Role</th>
+                        <th><button onClick={this.handleOpenAddPlayerModal.bind(this)}>+</button></th>
                     </tr>
                     </thead>
 
@@ -89,26 +100,59 @@ export default class Team extends Component {
                             <td>{player.number}</td>
                             <td>{player.role}</td>
                             <td><button onClick={this.deletePlayer.bind(this, player.id)}>Remove</button></td>
-                            <td><button onClick={this.handleOpenModal.bind(this, player)}>Update</button></td>
+                            <td><button onClick={this.handleOpenUpdatePlayerModal.bind(this, player)}>Update</button></td>
                         </tr>
                     )}
                     </tbody>
                 </table>
 
                 <ReactModal
-                    isOpen={this.state.showModal}
-                    contentLabel="Minimal Modal Example"
+                    isOpen={this.state.showUpdatePlayerModal}
+                    contentLabel="UpdatePlayerModal"
                 >
                     <form onSubmit={this.updatePlayer.bind(this)}>
-                        {/*<input type="text" value={this.state.value} onChange={this.handleChange.bind(this)} />*/}
-                        <input type="text" id="id" placeholder="Id" value={this.state.player.id} onChange={this.handleChange.bind(this)}/>
-                        <input type="text" id="name" placeholder="Name" value={this.state.player.name} onChange={this.handleChange.bind(this)}/>
-                        <input type="text" id="lastName" placeholder="Last Name" value={this.state.player.lastName} onChange={this.handleChange.bind(this)}/>
-                        <input type="text" id="number" placeholder="Number" value={this.state.player.number} onChange={this.handleChange.bind(this)}/>
-                        <input type="text" id="role" placeholder="Role" value={this.state.player.role} onChange={this.handleChange.bind(this)}/>
+                        <input type="text"
+                               id="id" placeholder="Id"
+                               value={this.state.player.id}
+                               onChange={this.handleChange.bind(this)}/>
+                        <input type="text"
+                               id="name"
+                               placeholder="Name"
+                               value={this.state.player.name}
+                               onChange={this.handleChange.bind(this)}/>
+                        <input type="text"
+                               id="lastName"
+                               placeholder="Last Name"
+                               value={this.state.player.lastName}
+                               onChange={this.handleChange.bind(this)}/>
+                        <input type="text"
+                               id="number"
+                               placeholder="Number"
+                               value={this.state.player.number}
+                               onChange={this.handleChange.bind(this)}/>
+                        <input type="text"
+                               id="role"
+                               placeholder="Role"
+                               value={this.state.player.role}
+                               onChange={this.handleChange.bind(this)}/>
 
-                        <button type="button" onClick={this.handleCloseModal.bind(this)}>Cancel</button>
-                        <button type="button" onClick={this.updatePlayer.bind(this)}>Save</button>
+                        <button type="button" onClick={this.handleCloseUpdatePlayerModal.bind(this)}>Cancel</button>
+                        <button type="submit">Save</button>
+                    </form>
+                </ReactModal>
+
+                <ReactModal
+                    isOpen={this.state.showAddPlayerModal}
+                    contentLabel="AddPlayerModal"
+                >
+                    <form id="lF" onSubmit={this.addPlayer.bind(this)}>
+                        <input type="text" placeholder="Name" ref={(input) => {this.nameInput = input}}/>
+                        <input type="text" placeholder="Last Name" ref={(input) => {this.lastNameInput = input}}/>
+                        <input type="text" placeholder="Number" ref={(input) => {this.numberInput = input}}/>
+                        <input type="text" placeholder="Role" ref={(input) => {this.roleInput = input}}/>
+
+                        <button type="button" onClick={this.handleCloseAddPlayerModal.bind(this)}>Cancel</button>
+                        <button type="submit">Save</button>
                     </form>
                 </ReactModal>
             </div>
